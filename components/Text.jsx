bc7198ff0b411b07c4cc06d327e82b3e9b2ff714
void-the-droid <bhuwan626282@gmail.com>
2024-08-27 22:23:52 +0545

    checcking deployementt

---
+++ components/Text.jsx
@@ -0,0 +1,14 @@
+
+const Text = () => {
+  return (
+    <div>
+      <div className="container">
+        <h2> Dolaji System</h2>
+        <p>The <strong>Dolaji system</strong>, also known as Dolaji Partha, was a distinctive social practice in ancient Nepal, particularly among the Newar community of the Kathmandu Valley. It featured a matrilocal residence arrangement, where the husband would move into the wife &apos;s household, and children were often considered part of the mother&apos;s family, influencing inheritance and family lineage.</p>
+        <p> This system reflected a more egalitarian or even matriarchal social structure, where women had significant control over household and family matters. Over time, the Dolaji system has largely faded, replaced by more patriarchal norms, but it remains an important part of the cultural history of the Newar people, illustrating diverse approaches to family, marriage, and gender roles.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Text
