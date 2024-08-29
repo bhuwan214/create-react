6011fa282fd1bd7fb8c2de11e413738633fc0ae8
void-the-droid <bhuwan626282@gmail.com>
2024-08-29 21:24:36 +0545

    ;

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -4,7 +4,7 @@ import {SeriesCard} from "./SeriesCard"
 export const NetflixSeries =()=>{
     return (
         <ul>
-            {data.map((curElem)=> <SeriesCard key={curElem.id} curElem={curElem}/>
+            { data.map((curElem)=> <SeriesCard key={curElem.id} curElem={curElem}/>
 )}
         </ul>
     )
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,20 +1,22 @@
 export const SeriesCard =(props)=>{
 
+    const {id, img_url,name,rating,description,genre,watch_url }=props.curElem
+   
     return(
-        <li key={props.curElem.id}>
+        <li key={id}>
+            {}
             <div>
-                {/* <img
-                src={props.curElem.img_url}
-                alt={props.curElem.name} 
-                height="40%"
-                width="40%"
-                /> */}
+                <img
+                src={img_url}
+                alt={name} 
+                width="200px"
+                />
             </div>
-            <h2>Name: {props.curElem.name}</h2>
-            <h3>Rating:{props.curElem.rating}</h3>
-            <p>Summary:{props.curElem.description}</p>
-            <p>Genre:{props.curElem.genre}</p>
-            <p>Cast:{props.curElem.watch_url}</p>
+            <h2>Name: {name}</h2>
+            <h3>Rating:{rating}</h3>
+            <p>Summary:{description}</p>
+            <p>Genre:{genre}</p>
+            <p>Cast:{watch_url}</p>
         </li>
         
     )
